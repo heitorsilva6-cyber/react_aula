bc28a11ef4d99386fd514beac094bddca416fec2
heitorsilva6-cyber <heitor.m.silva6@aluno.senai.br>
2026-04-24 08:59:07 -0300

    atividade

--- src/App.js
+++ src/App.js
@@ -1,13 +1,23 @@
 import './App.css';
-import Logo from './componentes/Logo' 
+
+import Header from './componentes/Header';
+import styled from 'styled-components';
+
+const AppContainer = `
+  height: 100vw;
+  width: 100vw;
+  background-image: linear-gradient(90deg, #002f52 35%, #326589);
+
+li{
+  list-style: none;
+}
+`
 
 function App() {
   return (
-    <div className='App'>
-      <header className='App-header'>
-        <Logo></Logo>
-      </header>
-    </div>
+    <AppContainer>
+      <Header/>
+    </AppContainer>
   );
 }
 
